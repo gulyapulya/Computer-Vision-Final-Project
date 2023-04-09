32608595064432b691168cbbe0c1818c401bef0c
Gulnur Baimukhambetova <gulnur2703@gmail.com>
2023-04-09 01:06:26 -0400

    Added config file

---
+++ config.py
@@ -0,0 +1,13 @@
+# frame dimensions
+#camera size 1920x1080
+# photo size 1008x756
+frame_w = 1008
+frame_h = 756
+
+# background dimensions
+# letter size 280x216
+background_w = 280
+background_h = 216
+
+#scale
+scale = 3
