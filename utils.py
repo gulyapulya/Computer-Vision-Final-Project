841184b3f7eedc0285c67fe9a6b215ffd944e24b
Gulnur Baimukhambetova <gulnur2703@gmail.com>
2023-04-09 01:05:40 -0400

    mend

--- utils.py
+++ utils.py
@@ -1,18 +1,19 @@
 import cv2
 import numpy as np
+import config 
 
 
-def getCountours(img, cannyThreshold=[100, 100], showResult=False, minArea = 1000, maxArea = 1000000, filter=0, draw=False):
+def getContours(img, cannyThreshold=[100, 100], showResult=False, minArea = 0, maxArea = 100, filter = 0, draw=False):
     '''
     Takes an image and returns an array of all contours found within that image sorted by size of the contour
     :param img: source image
     :param cannyThreshold: canny threshold
     :param showResult: Bool - displays the result of the function as an image
-    :param minArea: int - minimum area to accept as a contour
-    :param maxArea: int - maximum area to accept as a contour
-    :param filter: int - minimum number of corners
+    :param minArea: int (out of 100) - minimum percent of image area to accept as a contour
+    :param maxArea: int (out of 100) - maximum percent of image area to accept as a contour
+    :param filter: int - number of corners
     :param draw: Bool - draw the contours identified on the source image
-    :return: source image and the sorted array of contours
+    :return: the sorted array of contours
     '''
 
     # convert to grayscale
@@ -28,32 +29,31 @@ def getCountours(img, cannyThreshold=[100, 100], showResult=False, minArea = 100
 
     # display the result of the function
     if showResult:
-        myimg = cv2.resize(imgThreshold, (1920, 1080), interpolation=cv2.INTER_AREA)
-        cv2.imshow('Edges image', myimg)
+        cv2.imshow('Edges image', imgThreshold)
 
     # get the contours and process
-    contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     ret = []
     for contour in contours:
-        area = cv2.contourArea(contour)
+        img_w, img_h, _ = img.shape
+        area = cv2.contourArea(contour) / (img_w * img_h) * 100
+        #check if it is big enough and does not cover more than needed
         if minArea < area < maxArea:
             perimeter = cv2.arcLength(contour, True)
             cornerPoints = cv2.approxPolyDP(contour, 0.02*perimeter, True)
-            boundingBox = cv2.boundingRect(cornerPoints)
-
-            if filter > 0:
-                if len(cornerPoints) == filter:
-                    ret.append([len(cornerPoints), area, cornerPoints, boundingBox, contour])
-            else:
-                ret.append([len(cornerPoints), area, cornerPoints, boundingBox, contour])
+            #check number of corners
+            if filter > 0 and len(cornerPoints) != filter:
+                continue
+            boundingBox = cv2.minAreaRect(cornerPoints)
+            ret.append([len(cornerPoints), area, cornerPoints, boundingBox, contour])
+    
     # sort the return values based on the area of the contour found
     ret = sorted(ret, key=lambda x: x[1], reverse=True)
-
     if draw:
         for contour in ret:
             # draw the contour outline in red with a line thickness of 3
             cv2.drawContours(img, contour[4], -1, (0,0,255), 3)
-    return img, ret
+    return ret
 
 
 def reorder(points):
@@ -101,13 +101,28 @@ def warpImage(img, points, width, height, pad=0):
     imgWarp = imgWarp[pad:imgWarp.shape[0] - pad, pad:imgWarp.shape[1]-pad]
     return imgWarp
 
-def getDistance(pointA, pointB):
+def drawMeasurements(img, box):
     '''
-    Get the distance between two points in pixels using the pythagorian theorem
-    :param pointA:
-    :param pointB:
-    :return: the pixel distance between point A and point B
+    Draws measurements around the object
+    :param img: image to draw on
+    :param box: rotated rectangle
     '''
-    # sqrt((A_2-A_1)^2 + (B_2-B_1)^2)
-    return ((pointB[0]-pointA[0])**2 + (pointB[1]-pointA[1])**2)**0.5
+    # draw the lines
+    points = cv2.boxPoints(box)
+    points = np.int0(points)
+    cv2.arrowedLine(img, points[0], points[1], (0, 255, 0), 3, 8, 0, 0.05)
+    cv2.arrowedLine(img, points[0], points[3], (0, 255, 0), 3, 8, 0, 0.05)
+    # write the length and height in cm
+    (cx, cy), (w, h), angle = box
+    l = round(w /config.scale/10, 1)
+    h = round(h /config.scale/10, 1)
+    #draw measurements
+    #check if square
+    if min(l, h) / max(l, h) > 0.95:
+        avg = round(np.average([l, h]), 1)
+        cv2.putText(img, '{}cm'.format(avg), (points[0][0] - 40, points[0][1]),
+                cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (189, 159, 0), 1, cv2.LINE_AA)
+    else:
+        cv2.putText(img, '{}cm x {}cm'.format(l, h), (points[0][0] - 40, points[0][1]),
+                cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (189, 159, 0), 1, cv2.LINE_AA)
 
